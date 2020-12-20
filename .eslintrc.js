eb5fa7662dae11528fba26eedcd3134b7e58b3fa
codewithjalaj <jalaj@mail.com>
2020-12-20 20:35:56 +0530

    chore: :package: made eslint work with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,14 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
+  ignorePatterns: ['.eslintrc.js'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    // 'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -20,5 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
